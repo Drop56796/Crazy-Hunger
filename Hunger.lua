14b47c124cf859d62f64e208b5bf1783bf083176
opposition pantophobia <162717999+Drop56796@users.noreply.github.com>
2024-05-02 14:04:22 +0800

    Create Hunger.lua

---
+++ Hunger.lua
@@ -0,0 +1,72 @@
+require(game.Players.LocalPlayer.PlayerGui.MainUI.Initiator.Main_Game).caption("Where are you",true)
+wait(4)
+
+require(game.Players.LocalPlayer.PlayerGui.MainUI.Initiator.Main_Game).caption("I can see you",true)
+
+local Spawner = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Doors/Entity%20Spawner/V1/Source.lua"))()
+
+---====== Create entity ======---
+
+local entity = Spawner.createEntity({
+    CustomName = "Hunger",
+    Model = "https://github.com/catminetry/Hunger-V1/blob/main/Hunger.rbxm?raw=true", -- Your entity's model url here ("rbxassetid://1234567890" or GitHub raw url)
+    Speed = 1000,
+    MoveDelay = 1,
+    HeightOffset = 0,
+    CanKill = true,
+    KillRange = 10000,
+    SpawnInFront = true,
+    ShatterLights = true,
+    FlickerLights = {
+        Enabled = false,
+        Duration = 1
+    },
+    Cycles = {
+        Min = 1,
+        Max = 7,
+        Delay = 1
+    },
+    CamShake = {
+        Enabled = false,
+        Values = {1.5, 20, 0.1, 1},
+        Range = 100
+    },
+    ResistCrucifix = false,
+    BreakCrucifix = true,
+    DeathMessage = {"You die to Hunger", "if you meet it", "please hide", "when it going,", "you can continund..."},
+    IsCuriousLight = false
+})
+
+---====== Debug ======---
+
+entity.Debug.OnEntitySpawned = function()
+    wait(2) 
+    print("Entity has spawned")
+end
+
+entity.Debug.OnEntityDespawned = function()
+    wait(2)
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Drop56796/Hunger-Achievement/main/Hunger%20Achievement.lua"))()
+end
+
+entity.Debug.OnEntityStartMoving = function()
+    print("Entity started moving")
+end
+
+entity.Debug.OnEntityFinishedRebound = function()
+    print("Entity finished rebound")
+end
+
+entity.Debug.OnEntityEnteredRoom = function(room)
+    print("Entity entered room:", room)
+end
+
+entity.Debug.OnLookAtEntity = function()
+    print("Player looking at entity")
+end
+
+entity.Debug.OnDeath = function()
+    print("Player has died")
+end
+---====== Run entity ======---
+Spawner.runEntity(entity)
