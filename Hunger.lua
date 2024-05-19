56a18a168a68bfd53f2b00aca0596fafca37481a
seek lol.lua <162717999+Drop56796@users.noreply.github.com>
2024-05-19 14:49:52 +0800

    Update Hunger.lua

--- Hunger.lua
+++ Hunger.lua
@@ -37,9 +37,9 @@ local entity = Spawner.createEntity({
 entity.Debug.OnEntitySpawned = function()
     local cue2 = Instance.new("Sound")
 	cue2.Parent = game.Workspace
-	Instance.new = "Spawn"
+	cue2.Name = "Spawn"
 	cue2.SoundId = "rbxassetid://9125713501"
-	cue2.Volume = 999999998999
+	cue2.Volume = 99999999899
 	cue2.PlaybackSpeed = 0.5
 	cue2:Play()
     wait(2) 
