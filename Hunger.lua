ee476b5bc95b6ce4bc1fe0eced1e031dc497a61e
seek lol.lua <162717999+Drop56796@users.noreply.github.com>
2024-05-21 21:14:51 +0800

    Update Hunger.lua

--- Hunger.lua
+++ Hunger.lua
@@ -7,69 +7,116 @@ local cue2 = Instance.new("Sound")
 	cue2:Play()
 wait(32)
 
-local Spawner = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Doors/Entity%20Spawner/V1/Source.lua"))()
+local spawner = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Doors/Entity%20Spawner/V2/Source.lua"))()
 
----====== Create entity ======---
-
-local entity = Spawner.createEntity({
-    CustomName = "Hunger",
-    Model = "https://github.com/catminetry/Hunger-V1/blob/main/Hunger.rbxm?raw=true", -- Your entity's model url here ("rbxassetid://1234567890" or GitHub raw url)
-    Speed = 1000,
-    MoveDelay = 1,
-    HeightOffset = 0,
-    CanKill = true,
-    KillRange = 10000,
-    SpawnInFront = true,
-    ShatterLights = true,
-    FlickerLights = {
-        Enabled = false,
-        Duration = 1
-    },
-    Cycles = {
-        Min = 1,
-        Max = 7,
-        Delay = 1
-    },
-    CamShake = {
-        Enabled = false,
-        Values = {1.5, 20, 0.1, 1},
-        Range = 100
-    },
-    ResistCrucifix = false,
-    BreakCrucifix = true,
-    DeathMessage = {"You die to Hunger", "if you meet it", "please hide", "when it going,", "you can continund..."},
-    IsCuriousLight = false
+local entity = spawner.Create({
+	Entity = {
+		Name = "Hungers",
+		Asset = "https://github.com/catminetry/Hunger-V1/blob/main/Hunger.rbxm?raw=true",
+		HeightOffset = 0
+	},
+	Lights = {
+		Flicker = {
+			Enabled = true,
+			Duration = 1
+		},
+		Shatter = true,
+		Repair = false
+	},
+	CameraShake = {
+		Enabled = true,
+		Range = 100,
+		Values = {1.5, 20, 0.1, 1} -- Magnitude, Roughness, FadeIn, FadeOut
+	},
+	Movement = {
+		Speed = 1500,
+		Delay = 1,
+		Reversed = false
+	},
+	Rebounding = {
+		Enabled = true,
+		Type = "Ambush", -- "Blitz"
+		Min = 3,
+		Max = 6,
+		Delay = 0
+	},
+	Damage = {
+		Enabled = true,
+		Range = 40,
+		Amount = 125
+	},
+	Crucifixion = {
+		Enabled = true,
+		Range = 40,
+		Resist = false,
+		Break = true
+	},
+	Death = {
+		Type = "Guiding", -- "Curious"
+		Hints = {"You die to Hungers", "care about it..", "It is difficult", "Just hide!!"},
+		Cause = "Hungers"
+	}
 })
 
----====== Debug ======---
+---====== Debug entity ======---
 
-entity.Debug.OnEntitySpawned = function 
+entity:SetCallback("OnSpawned", function()
     print("Entity has spawned")
-end
+end)
 
-entity.Debug.OnEntityDespawned = function()
-    wait(2)
+entity:SetCallback("OnStartMoving", function()
+    print("Entity has started moving")
+end)
+
+entity:SetCallback("OnReachNode", function(node)
+	print("Entity has reached node:", node)
+end)
+
+entity:SetCallback("OnEnterRoom", function(room, firstTime)
+    if firstTime == true then
+        print("Entity has entered room: ".. room.Name.. " for the first time")
+    else
+        print("Entity has entered room: ".. room.Name.. " again")
+    end
+end)
+
+entity:SetCallback("OnLookAt", function(lineOfSight)
+	if lineOfSight == true then
+		print("Player is looking at entity")
+	else
+		print("Player view is obstructed by something")
+	end
+end)
+
+entity:SetCallback("OnRebounding", function(startOfRebound)
+    if startOfRebound == true then
+        print("Entity has started rebounding")
+	else
+        print("Entity has finished rebounding")
+	end
+end)
+
+entity:SetCallback("OnDespawning", function()
+    print("Entity is despawning")
+end)
+
+entity:SetCallback("OnDespawned", function()
+    print("Entity has despawned")
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Drop56796/Hunger-Achievement/main/Hunger%20Achievement.lua"))()
-end
+end)
 
-entity.Debug.OnEntityStartMoving = function()
-    print("Entity started moving")
-end
+entity:SetCallback("OnDamagePlayer", function(newHealth)
+	if newHealth == 0 then
+		print("Entity has killed the player")
+	else
+		print("Entity has damaged the player")
+	end
+end)
 
-entity.Debug.OnEntityFinishedRebound = function()
-    print("Entity finished rebound")
-end
-
-entity.Debug.OnEntityEnteredRoom = function(room)
-    print("Entity entered room:", room)
-end
-
-entity.Debug.OnLookAtEntity = function()
-    print("Player looking at entity")
-end
-
-entity.Debug.OnDeath = function()
-    print("Player has died")
-end
 ---====== Run entity ======---
-Spawner.runEntity(entity)
+
+entity:Run()
+-- entity:Pause()
+-- entity:Resume()
+-- entity:IsPaused()
+-- entity:Despawn()
